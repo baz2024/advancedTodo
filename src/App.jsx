55568f2a27f958747cc11b689ce71ea308f812cd
Baz2024 <basel.magableh@dbs.ie>
2025-04-15 20:19:05 +0100

    add projects files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  const isLoggedIn = !!localStorage.getItem('user');
+
+  return (
+    <Routes>
+      <Route path="/" element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login />} />
+      <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default App;
